06f5a5bb2e33a6f5dd5e77f20b7cb6f1469c1174
Timothy Dykes <timothywanedykes@gmail.com>
2022-01-04 22:58:43 -0800

    Add useState, useCallback and fetch for rendering mealAPI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { useState, useCallback } from 'react';
+
 import './App.css';
 
 function App() {
+  const [meal, setMeal] = useState(null);
+
+  const fetchMealHandler = useCallback( async () => {
+
+    try {
+      const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+
+      if (response.status !== 200) {
+        throw new Error ('Something backfired!');
+      }
+
+      const data = await response.json();
+      const meal = data.meals[0];
+      console.log(meal)
+
+      const trimmedMealObj = {};
+
+      for ( const property in meal ) {
+        if (`${meal[property]}`) {
+          //console.log(`${property} `, `${meal[property]} ` )
+          trimmedMealObj[`${property}`] = `${meal[property]}`;
+        }
+      }
+      console.log(trimmedMealObj);
+
+    }  catch (error) {
+      console.log(error.message)
+    }
+  }, []);
+
   return (
     <div className="App">
       <h2>This is the mealAPI app</h2>
+      <button onClick={fetchMealHandler}>Click for new recipe!</button>
     </div>
   );
 }
