21233f37e36af8102e93221d272ba4c21577bf5b
Timothy Dykes <timothywanedykes@gmail.com>
2022-01-06 16:21:56 -0800

    Refactor with to include thumbnail component

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
-import MealIngredients from './MealIngredients'
+import MealIngredients from './MealIngredients';
+import MealThumbnail from './MealThumbnail';
 
 const Meal = (props) => {
   const meal = props.meal;
 
+  console.log(meal)
+  let content = <p></p>
+  let thumbnail = <p></p>
+
+  if(meal) {
+    content = <div>
+      <h2>{meal.strMeal}</h2>
+    </div>;
+    thumbnail = <MealThumbnail image={meal.strMealThumb}/>;
+  }
+
   return (
-    <section>
-      <MealIngredients items={meal} />
-    </section>
+    <div>
+      <section>
+        {thumbnail}
+      </section>
+      <section>
+        {content}
+      </section>
+      <section>
+        <MealIngredients items={meal} />
+      </section>
+    </div>
   )
 };
 
