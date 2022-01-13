cfddb2f771064427c8b8c39de61e24183b6726e2
Timothy Dykes <timothywanedykes@gmail.com>
2022-01-12 20:23:29 -0800

    refactor rendering for better readability

--- src/components/MealInstructions.js
+++ src/components/MealInstructions.js
@@ -3,12 +3,25 @@ import React from 'react';
 import './MealInstructions.css';
 
 const MealInstructions = (props) => {
+  let whereToSplit = '';
+
+  if (props.instructions.includes('\r\n')) {
+    whereToSplit = '\r\n';
+  } else if (props.instructions.includes('.')) {
+    whereToSplit = '.';
+  }
+
+  let split = props.instructions.split(whereToSplit)
+  console.log(split)
 
   return (
     <div className="instructions">
-      <p>{props.instructions}</p>
+      {split.map(step => {
+        return (
+          step ? <li>{step}</li> : <p></p>)
+      })}
     </div>
   )
 };
-
+//<p>{props.instructions}</p>
 export default MealInstructions;
